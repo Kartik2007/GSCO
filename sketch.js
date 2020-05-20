042e750dcb73af4ec810c0fbd1ced80d02a30cc6
Kartik2007 <64688178+Kartik2007@users.noreply.github.com>
2020-05-20 18:01:50 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+
+var car,wall ;
+var speed,weight;
+function setup() {
+  createCanvas(800,400);
+  car = createSprite(50, 200, 50, 50);
+  wall=createSprite(1500,200,60,height/2)
+  speed=random(54,96)
+  weight=random(400,1400)
+  wall.shapeColor= "black";
+  
+  car.velocityX = speed;
+  
+
+
+  if(wall.x-car.x<(car.width+wall.width/2))
+  {
+  car.velocityX = 0;
+  var deformation = 0.5*weigth*speed*speed/22500
+  if (deformation>180){
+  
+
+  
+
+  car.shapeColor = color("red");
+  }
+}
+
+if(deformation<100){}
+
+  car.shapeColor= color("green")}
+
+
+function draw() {
+  background(0);
+  
+    
+  
+
+  
+  
+
+
+  drawSprites();
+}
